9f5747c61dacd85a120f0686bc5e92c8dd5eec2a
Dan Hoying <danhoying@gmail.com>
2017-09-05 11:30:43 -0700

    Get screen resolution and set view

--- app/src/main/java/com/example/dan/platformgame/PlatformActivity.java
+++ app/src/main/java/com/example/dan/platformgame/PlatformActivity.java
@@ -1,7 +1,31 @@
 package com.example.dan.platformgame;
 
+import android.graphics.Point;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 
 public class PlatformActivity extends AppCompatActivity {
+
+    // The object to handle the View
+    private PlatformView platformView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get a Display object to access screen details
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Load the resolution into a Point object
+        Point resolution = new Point();
+        display.getSize(resolution);
+
+        // Set view for game using resolution object
+        platformView = new PlatformView(this, resolution.x, resolution.y);
+
+        // Make our platformView the view for the Activity
+        setContentView(platformView);
+    }
 
 }
