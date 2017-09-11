9f0437b806223d6d1c7a1972628c97a1100fe3f4
Dan Hoying <danhoying@gmail.com>
2017-09-11 09:30:57 -0700

    Finish basic outline of PlatformView class

--- app/src/main/java/com/example/dan/platformgame/PlatformView.java
+++ app/src/main/java/com/example/dan/platformgame/PlatformView.java
@@ -2,7 +2,9 @@ package com.example.dan.platformgame;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -33,5 +35,48 @@ public class PlatformView extends SurfaceView implements Runnable {
         // Initialize drawing objects
         ourHolder = getHolder();
         paint = new Paint();
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            startFrameTime = System.currentTimeMillis();
+
+            update();
+            draw();
+
+            // Calculate the fps this frame to time animations and movement
+            timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if (timeThisFrame >= 1) {
+                fps = 1000 / timeThisFrame;
+            }
+        }
+    }
+
+    private void draw() {
+        if (ourHolder.getSurface().isValid()) {
+            canvas = ourHolder.lockCanvas();
+            paint.setColor(Color.argb(255, 0, 0, 255));
+            canvas.drawColor(Color.argb(255, 0, 0, 255));
+
+            // New drawing code here
+
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void pause() {
+        running = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            Log.e("error", "failed to pause thread");
+        }
+    }
+
+    public void resume() {
+        running = true;
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 }
