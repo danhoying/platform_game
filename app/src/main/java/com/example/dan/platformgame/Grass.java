98b14fafd17526748f28f194c4b4ec1ec5f15e49
Dan Hoying <danhoying@gmail.com>
2017-09-12 11:50:51 -0700

    Initialize size and location of Grass class

---
+++ app/src/main/java/com/example/dan/platformgame/Grass.java
@@ -0,0 +1,17 @@
+package com.example.dan.platformgame;
+
+public class Grass extends GameObject {
+
+    Grass(float worldStartX, float worldStartY, char type) {
+        final float HEIGHT = 1;
+        final float WIDTH = 1;
+
+        setHeight(HEIGHT);
+        setWidth(WIDTH);
+        setType(type);
+        setBitmapName("turf");
+        setWorldLocation(worldStartX, worldStartY, 0);
+    }
+
+    public void update(long fps, float gravity) {}
+}
