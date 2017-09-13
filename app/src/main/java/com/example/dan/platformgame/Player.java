3495c31360ea37cbd26316e0b2ddec8aa33f9a57
Dan Hoying <danhoying@gmail.com>
2017-09-13 09:27:13 -0700

    Initialize Player class variables

--- app/src/main/java/com/example/dan/platformgame/Player.java
+++ app/src/main/java/com/example/dan/platformgame/Player.java
@@ -1,4 +1,18 @@
 package com.example.dan.platformgame;
 
-public class Player {
+import android.content.Context;
+
+public class Player extends GameObject {
+    Player(Context context, float worldStartX, float worldStartY, int pixelsPerMetre) {
+        final float HEIGHT = 2;
+        final float WIDTH = 1;
+        setHeight(HEIGHT);
+        setWidth(WIDTH);
+        setType('p');
+        setBitmapName("player");
+        setWorldLocation(worldStartX, worldStartY, 0);
+    }
+    public void update(long fps, float gravity) {
+
+    }
 }
