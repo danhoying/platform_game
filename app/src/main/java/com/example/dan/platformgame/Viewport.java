aa2c1354932aa5219e965cd4797c768274a9cbcc
Dan Hoying <danhoying@gmail.com>
2017-09-19 08:29:09 -0700

    Create Viewport class and constructor

--- app/src/main/java/com/example/dan/platformgame/Viewport.java
+++ app/src/main/java/com/example/dan/platformgame/Viewport.java
@@ -1,4 +1,34 @@
 package com.example.dan.platformgame;
 
+import android.graphics.Rect;
+
 public class Viewport {
+    private Vector2Point5D currentViewportWorldCentre;
+    private Rect convertedRect;
+    private int pixelsPerMetreX;
+    private int pixelsPerMetreY;
+    private int screenXResolution;
+    private int screenYResolution;
+    private int screenCentreX;
+    private int screenCentreY;
+    private int metresToShowX;
+    private int metresToShowY;
+    private int numClipped;
+
+    Viewport(int x, int y) {
+        screenXResolution = x;
+        screenYResolution = y;
+
+        screenCentreX = screenXResolution / 2;
+        screenCentreY = screenYResolution / 2;
+
+        pixelsPerMetreX = screenXResolution / 32;
+        pixelsPerMetreY = screenYResolution / 18;
+
+        metresToShowX = 34;
+        metresToShowY = 20;
+
+        convertedRect = new Rect();
+        currentViewportWorldCentre = new Vector2Point5D();
+    }
 }
